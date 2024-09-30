0a21982133ae152dd687899d657b4830b950305c
結友 <miagetegorann@gmail.com>
2024-09-30 11:44:57 +0900

    gozen

--- pro2kojin/FileName.c
+++ pro2kojin/FileName.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
+#include <direct.h>
+#include <string.h>
+#include <stdlib.h>
 
+//コンパイルしてコマンドラインから実行してください
+//引数はreadmeを参照
 void initRepository()
 {
-
+	if (_mkdir("./test") == 0)
+	{
+		puts("success!!");
+	}
+	else
+	{
+		printf("エラー\nリポジトリを作成できませんでした\nすでに存在するか、アクセス権がありません");
+	}
 }
 
 void addFile()
@@ -12,6 +24,29 @@ void addFile()
 
 int main(void)
 {
+	char command[256];
+	char* input;
 
-	return 0;
+	printf("なんちゃってバージョン管理\n");
+	while (1)
+	{
+		printf("> ");
+		input = fgets(command, sizeof(command), stdin);
+		
+		command[strcspn(command, "\n")] = 0;
+
+		if (strcmp(command, "init") == 0);
+		{
+			initRepository();
+		}
+	
+	
+	}
+
+
+
+	
+
+	initRepository();
+	
 }
